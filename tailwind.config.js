a9476c3a14c63f8638258381613134577c0d96ce
Gustavo <143291534+Gusta2409@users.noreply.github.com>
2025-12-31 02:36:08 -0300

    Ref: #ajuste tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./index.tsx",
+    "./App.tsx",
+    "./types.ts",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./services/**/*.{js,ts,jsx,tsx}"
+  ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+      }
+    },
   },
   plugins: [],
 }
